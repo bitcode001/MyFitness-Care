bcae237d2842f11e020b35be11eddfc085f1674f
bitcode001 <anil.rai00101@gmail.com>
2023-07-26 01:24:22 +0100

    feat(): Added formik and yup for form control and enhancement in user auth

--- src/screens/onboarding/login.screen.tsx
+++ src/screens/onboarding/login.screen.tsx
@@ -1,43 +1,53 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {View, Text, Image} from 'react-native';
 import {StackNavigationProp} from '@react-navigation/stack';
-import {RouteProp} from '@react-navigation/native';
+// import {RouteProp} from '@react-navigation/native';
 import {OnboardingStackParamList} from '.';
 import {Button, TextInput} from 'react-native-paper';
 import SafeAreaScrollView from '@/components/SafeAreaScrollView';
 import Toast from 'react-native-toast-message';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
+import ErrorLabel from '@/components/Form/ErrorLabel';
 
 type LoginScreenNavigationProp = StackNavigationProp<
   OnboardingStackParamList,
   'Login'
 >;
-type LoginScreenRouteProp = RouteProp<OnboardingStackParamList, 'Login'>;
+// type LoginScreenRouteProp = RouteProp<OnboardingStackParamList, 'Login'>;
 
 type LoginInterface = {
   navigation: LoginScreenNavigationProp;
-  route: LoginScreenRouteProp;
+  // route: LoginScreenRouteProp;
 };
 
-export default function LoginScreen({
-  navigation,
-  route,
-}: LoginInterface): JSX.Element {
+const loginValidation = Yup.object().shape({
+  email: Yup.string()
+    .email('Please enter valid email')
+    .required('Email Address is Required'),
+  password: Yup.string()
+    .min(6, ({min}) => `Password must be at least ${min} characters`)
+    .required('Password is required'),
+});
+
+interface InitialLoginFormInterface {
+  email: string;
+  password: string;
+}
+
+export default function LoginScreen({navigation}: LoginInterface): JSX.Element {
   const [passwordVisible, setPasswordVisible] = React.useState(false);
   const togglePasswordVisibility = () => setPasswordVisible(!passwordVisible);
 
-  const handleLogin = () => {
+  const handleLogin = (values: InitialLoginFormInterface) => {
+    console.log(values);
     Toast.show({
       type: 'success',
-      text1: 'Hello',
-      text2: 'This is some something 👋',
+      text1: 'Success',
+      text2: 'You have successfully logged in!',
     });
   };
 
-  useEffect(() => {
-    console.log('LoginScreen');
-    // console.log('navigation', navigation);
-    // console.log('route', route);
-  }, [navigation, route]);
   return (
     <SafeAreaScrollView>
       <View className="flex flex-col justify-center items-center">
@@ -50,31 +60,51 @@ export default function LoginScreen({
           Lets learn to love ourselves first!
         </Text>
       </View>
-      <View className="my-5">
-        <TextInput
-          className="mt-5"
-          outlineColor="transparent"
-          label={'Email'}
-          mode="outlined"
-        />
-        <TextInput
-          className="mt-5"
-          outlineColor="transparent"
-          label={'Password'}
-          secureTextEntry={!passwordVisible}
-          mode="outlined"
-          right={
-            <TextInput.Icon
-              onPress={() => togglePasswordVisibility()}
-              icon="eye"
-            />
-          }
-        />
-      </View>
+      <Formik
+        initialValues={{email: '', password: ''}}
+        validationSchema={loginValidation}
+        onSubmit={(values: InitialLoginFormInterface) => handleLogin(values)}>
+        {({handleChange, handleBlur, handleSubmit, values}) => (
+          <React.Fragment>
+            <View className="my-5">
+              <TextInput
+                className="mt-5"
+                outlineColor="transparent"
+                label={'Email'}
+                mode="outlined"
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+              />
+              <ErrorLabel fieldKey="email" />
+              <TextInput
+                className="mt-5"
+                outlineColor="transparent"
+                label={'Password'}
+                secureTextEntry={!passwordVisible}
+                mode="outlined"
+                right={
+                  <TextInput.Icon
+                    onPress={() => togglePasswordVisibility()}
+                    icon="eye"
+                  />
+                }
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+              />
+              <ErrorLabel fieldKey="password" />
+            </View>
 
-      <Button mode="contained" className="mt-10" onPress={() => handleLogin()}>
-        <Text>Sign In</Text>
-      </Button>
+            <Button
+              mode="contained"
+              className="mt-10"
+              onPress={() => handleSubmit()}>
+              <Text>Sign In</Text>
+            </Button>
+          </React.Fragment>
+        )}
+      </Formik>
       <Button
         mode="outlined"
         className="mt-5"
--- src/screens/onboarding/register.screen.tsx
+++ src/screens/onboarding/register.screen.tsx
@@ -6,108 +6,139 @@ import {Button, TextInput} from 'react-native-paper';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {OnboardingStackParamList} from '.';
-import {RouteProp, useNavigationState} from '@react-navigation/native';
+// import {RouteProp} from '@react-navigation/native';
 import Toast from 'react-native-toast-message';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
+import ErrorLabel from '@/components/Form/ErrorLabel';
 
 type RegisterScreenNavigationProp = StackNavigationProp<
   OnboardingStackParamList,
   'Register'
 >;
 
-type RegisterScreenRouteProp = RouteProp<OnboardingStackParamList, 'Register'>;
+// type RegisterScreenRouteProp = RouteProp<OnboardingStackParamList, 'Register'>;
 
 type RegisterScreenProps = {
   navigation: RegisterScreenNavigationProp;
-  route: RegisterScreenRouteProp;
+  // route: RegisterScreenRouteProp;
 };
+
+const registerValidation = Yup.object().shape({
+  username: Yup.string().required('Username is Required'),
+  email: Yup.string()
+    .email('Please enter valid email')
+    .required('Email Address is Required'),
+  password: Yup.string()
+    .min(6, ({min}) => `Password must be at least ${min} characters`)
+    .required('Password is required'),
+});
+
+interface RegisterFieldsInitialInterface {
+  username: string;
+  email: string;
+  password: string;
+}
 
 export default function RegisterScreen({
   navigation,
-  route,
 }: RegisterScreenProps): JSX.Element {
   const [passwordVisible, setPasswordVisible] = React.useState(false);
   const togglePasswordVisibility = () => setPasswordVisible(!passwordVisible);
-  const state = useNavigationState(state => state);
-  const handleRegister = () => {
+  const handleRegister = (values: RegisterFieldsInitialInterface) => {
     Toast.show({
       type: 'success',
       text1: 'Success',
       text2: 'You have successfully registered!',
     });
-    // navigation.navigate('Login');
-    navigation.popToTop();
+    console.log('Registering values: ', values);
   };
   const handleBackPress = () => {
-    // console.log('This is navigate: ', navigation);
-    // navigation.goBack();
-    // navigation.navigate('Login');
-    // console.log('Can go back: ', navigation.popToTop());
-    console.log('Nav State: ', state);
+    navigation.goBack();
   };
-
-  React.useEffect(() => {
-    console.log('This is route: ', route);
-  }, [route]);
 
   return (
     <SafeAreaScrollView>
-      <View>
-        <Button
-          mode="text"
-          className="rounded-full w-10"
-          style={{backgroundColor: MThemeColors.black}}
-          onPress={() => handleBackPress()}>
-          <MaterialIcon
-            name="arrow-left"
-            size={22}
-            color={MThemeColors.white}
-          />
-        </Button>
-        <View className="flex flex-col justify-center items-center">
-          <Image
-            className="h-40 w-40"
-            source={require('@/assets/icons/fitness.png')}
-          />
-          <Text className="text-lg font-medium">My Fitness Care</Text>
-          <Text className="text-base font-semibold">
-            Lets learn to love ourselves first!
-          </Text>
-        </View>
-        <View className="my-5">
-          <TextInput
-            className="mt-5"
-            outlineColor="transparent"
-            label={'Username'}
-            mode="outlined"
-          />
-          <TextInput
-            className="mt-5"
-            outlineColor="transparent"
-            label={'Email'}
-            mode="outlined"
-          />
-          <TextInput
-            className="mt-5"
-            outlineColor="transparent"
-            label={'Password'}
-            secureTextEntry={!passwordVisible}
-            mode="outlined"
-            right={
-              <TextInput.Icon
-                onPress={() => togglePasswordVisibility()}
-                icon="eye"
-              />
-            }
-          />
-        </View>
-
-        <Button
-          mode="contained"
-          className="mt-10"
-          onPress={() => handleRegister()}>
-          <Text>Register</Text>
-        </Button>
+      <Button
+        mode="text"
+        className="rounded-full w-10"
+        style={{backgroundColor: MThemeColors.black}}
+        onPress={() => handleBackPress()}>
+        <MaterialIcon name="arrow-left" size={22} color={MThemeColors.white} />
+      </Button>
+      <View className="flex flex-col justify-center items-center">
+        <Image
+          className="h-40 w-40"
+          source={require('@/assets/icons/fitness.png')}
+        />
+        <Text className="text-lg font-medium">My Fitness Care</Text>
+        <Text className="text-base font-semibold">
+          Lets learn to love ourselves first!
+        </Text>
       </View>
+      <Formik
+        initialValues={{
+          username: '',
+          email: '',
+          password: '',
+        }}
+        validationSchema={registerValidation}
+        onSubmit={(values: RegisterFieldsInitialInterface) =>
+          handleRegister(values)
+        }>
+        {({handleChange, handleBlur, handleSubmit, values}) => (
+          <React.Fragment>
+            <View className="my-5">
+              <TextInput
+                className="mt-5"
+                outlineColor="transparent"
+                label={'Username'}
+                mode="outlined"
+                onChangeText={handleChange('username')}
+                onBlur={handleBlur('username')}
+                value={values.username}
+              />
+              <ErrorLabel fieldKey="username" />
+
+              <TextInput
+                className="mt-5"
+                outlineColor="transparent"
+                label={'Email'}
+                mode="outlined"
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+              />
+              <ErrorLabel fieldKey="email" />
+
+              <TextInput
+                className="mt-5"
+                outlineColor="transparent"
+                label={'Password'}
+                secureTextEntry={!passwordVisible}
+                mode="outlined"
+                right={
+                  <TextInput.Icon
+                    onPress={() => togglePasswordVisibility()}
+                    icon="eye"
+                  />
+                }
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+              />
+              <ErrorLabel fieldKey="password" />
+            </View>
+
+            <Button
+              mode="contained"
+              className="mt-10"
+              onPress={() => handleSubmit()}>
+              <Text>Register</Text>
+            </Button>
+          </React.Fragment>
+        )}
+      </Formik>
     </SafeAreaScrollView>
   );
 }
